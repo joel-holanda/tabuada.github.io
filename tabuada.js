ad13016e28974a5136bc4b3d95b763fc4f7de64c
joel-holanda <joelholanda45@gmail.com>
2021-11-11 12:11:49 -0300

    Primeira versao da tabuada

---
+++ tabuada.js
@@ -0,0 +1,25 @@
+function remove(){
+var testData = !!document.querySelector('h1');
+if(testData == true ){
+    var tag = document.querySelector('h1');
+    tag.parentNode.removeChild(tag);
+}
+}
+function apagar(){
+    for (a = 1; 10 >= a; a++){
+        remove()
+    }
+}
+
+function tabu(){    
+        apagar()
+    for (a = 1; 10 >= a; a++){
+        var valor = document.getElementById('pegaValor').value;
+        var multiplicar = a * valor
+        var corpo = document.body.querySelector('div');
+        var elemento = document.createElement('h1');
+        var fator = document.createTextNode(a + " X " + valor + " = " + multiplicar);
+        elemento.appendChild(fator);
+        corpo.appendChild(elemento);
+    }    
+}
